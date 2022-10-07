80a8e6bfc952896ab75dff7130328729b91962dd
sebas velasco <11669998+sebasvelasco353@users.noreply.github.com>
2022-10-07 14:42:42 -0500

    Getting data obj from csv

--- README.md
+++ README.md
@@ -22,3 +22,7 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+### Data sources:
+- [Domestic Violence in Colombia](https://www.kaggle.com/datasets/oscardavidperilla/domestic-violence-in-colombia?resource=download)
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,11 +8,17 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import * as d3 from "d3";
 
 export default {
   name: 'HomeView',
   components: {
     HelloWorld
-  }
+  },
+  async mounted() {
+    const data = await d3.csv("/data.csv");
+    console.log(data);
+    console.log("----------------------------------------------------- here -----------------------------");
+  },
 }
 </script>
